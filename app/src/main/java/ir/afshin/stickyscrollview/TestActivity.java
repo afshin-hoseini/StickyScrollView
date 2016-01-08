77f2108d9fb7abc415a7942bab2f67a00a065833
Afshin Hoseini <afshin.hoseini@gmail.com>
2016-01-08 16:06:29 +0330

    The sticky scrollView is ready.
    Just created sticky scrollView which can only have one sticky header inside. In few next steps I start to support vertically scrollable views inside the stickyscrollview. :)

--- app/src/main/java/ir/afshin/stickyscrollview/TestActivity.java
+++ app/src/main/java/ir/afshin/stickyscrollview/TestActivity.java
@@ -3,11 +3,14 @@ package ir.afshin.stickyscrollview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ir.afshin.stickyscrollviewlib.StickyScrollView;
+
 public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_test);
     }
 }
